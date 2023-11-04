f01abc875aba4f064fae2225f51d34b678f42bf7
Darsh-KP <pateldarsh919@gmail.com>
2023-11-04 16:21:05 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react"
 import './App.css';
 import logo from "./logo1.png"
+
+import { BrowserRouter as Router, Swtich, Route } from 'react-router-dom'
+import Navbar from "./components/navbar"
+
 function App() {
 	return (
 		<>
+			<router>
+				<Navbar />
+				<Switch>
+					<Route path="/" exact/>
+				</Switch>
+			</router>
 			<body>
   				<h1>SailSprint</h1>
 				<img src={logo}/>
 			</body>
-
 		</>
 	)
 }
