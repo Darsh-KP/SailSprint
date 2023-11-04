b4936dfaa137ad68a800cd6f934cf9be79b4ecb2
VarunShah01 <67612886+VarunShah01@users.noreply.github.com>
2023-11-04 15:59:16 -0400

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react"
-
+import './App.css';
+import logo from "./logo1.png"
 function App() {
 	const [count, setCount] = useState(0)
 
 	return (
 		<>
-			<p>hello</p>
+			<body>
+  				<h1>SailSprint</h1>
+				<img src={logo}/>
+			</body>
+
 		</>
 	)
 }
