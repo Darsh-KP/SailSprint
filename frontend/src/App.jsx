acf4b507499ddfbf9099883efd9b71fcc518a47b
Darsh-KP <pateldarsh919@gmail.com>
2023-11-04 16:45:26 -0400

    Nav bar template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,13 @@ import { useState } from "react"
 import './App.css';
 import logo from "./logo1.png"
 
-import { BrowserRouter as Router, Swtich, Route } from 'react-router-dom'
+import { BrowserRouter as router, Routes, Route } from 'react-router-dom'
 import Navbar from "./components/navbar"
 
 function App() {
 	return (
 		<>
-			<router>
-				<Navbar />
-				<Switch>
-					<Route path="/" exact/>
-				</Switch>
-			</router>
+			<div><Navbar /></div>
 			<body>
   				<h1>SailSprint</h1>
 				<img src={logo}/>
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -7,9 +7,7 @@ function Navbar() {
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    <Link to="/" className='navbar-logo'>
-                        Hello
-                    </Link>
+                    Hello
                 </div>
             </nav>
         </>
