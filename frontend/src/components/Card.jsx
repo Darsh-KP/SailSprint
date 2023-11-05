5d306fd359f7599e87a1fb8ff8ddfb9ab212c1ee
justin <justinbautista923@gmail.com>
2023-11-04 23:07:45 -0400

    basic cards

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react"
 
-export default function Card() {
+export default function Card({ event }) {
 	return (
 		<div>
-			<p>hello</p>
+            <h1>{event.event_name}</h1>
+            <p>{event.hostname}</p>
+            <p>Location</p>
+            <p>{event.date_time}</p>
+            <p>{event.price}</p>
+            <button>Join as Racer</button>
+            <button>Join as Viewer</button>
 		</div>
 	)
 }
