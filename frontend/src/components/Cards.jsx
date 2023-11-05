10da1c05abcc12b1dfbd5f4d960b5c78c3a162cc
justin <justinbautista923@gmail.com>
2023-11-04 23:25:56 -0400

    updated cards

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -7,25 +7,29 @@ export default function Cards() {
 
 	useEffect(() => {
 		const getEvents = async () => {
-			const allEvents = await api.get("/events")
-			let allEventsWithCount = []
-
-			for (const event of allEvents) {
-				const viewerCount = await api.get(`/viewers/${event.event_id}`)
-				const racerCount = await api.get(`/racers/${event.event_id})`)
-                const hostname = await api.get(`/users/getUsername/${event.host_id}`)
-
-				const eventWithCounts = {
-					...event,
-					viewerCount: viewerCount,
-					racerCount: racerCount,
-                    hostname: hostname
-				}
-
-				allEventsWithCount.push(eventWithCounts)
-			}
-
-			setEvents(allEventsWithCount)
+            try {
+                const allEvents = await api.get("/events")
+                let allEventsWithCount = []
+    
+                for (const event of allEvents) {
+                    const viewerCount = await api.get(`/viewers/${event.event_id}`)
+                    const racerCount = await api.get(`/racers/${event.event_id})`)
+                    const hostname = await api.get(`/users/getUsername/${event.host_id}`)
+    
+                    const eventWithCounts = {
+                        ...event,
+                        viewerCount: viewerCount,
+                        racerCount: racerCount,
+                        hostname: hostname
+                    }
+    
+                    allEventsWithCount.push(eventWithCounts)
+                }
+    
+                setEvents(allEventsWithCount)
+            } catch (err) {
+                console.error(err)
+            }
 		}
 
         getEvents()
