52f4908265d19639248115715b101e036685c74f
Darsh-KP <pateldarsh919@gmail.com>
2023-11-04 21:00:48 -0400

    Conflict resolve

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,24 @@
 import { useState } from "react"
 import './App.css';
-import logo from "./logo1.png"
 import { BrowserRouter as router, Routes, Route } from 'react-router-dom'
 import Navbar from "./components/navbar"
+
+// Image imports for the home page
+import boat from "./assets/Sailboat.png"
+
 import { SignInButton } from "@clerk/clerk-react";
 
 function App() {
 	return (
 		<>
-			<Navbar />
-			<h1>SailSprint</h1>
-			<img src={logo}/>
+			<div><Navbar /></div>
+			<body>
+				<div className="overlay">
+					<img src={boat} className="home-boat" />
+				</div>
+  				
+			</body>
 			<SignInButton />
-			<p>ioahsdoihasd</p>
 		</>
 	)
 }
