c60f38fe2e2d02fb8c15b6fb2fe5d064e457f5dd
justin <justinbautista923@gmail.com>
2023-11-05 10:48:35 -0500

    added mock events again

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -7,69 +7,69 @@ export default function Cards() {
 	const [events, setEvents] = useState([])
 
 	useEffect(() => {
-		const getEvents = async () => {
-            try {
-                const allEvents = await api.get("/events")
-                let allEventsWithCount = []
+		const getEvents = () => {
+            // try {
+            //     const allEvents = await api.get("/events")
+            //     let allEventsWithCount = []
     
-                for (const event of allEvents) {
-                    const viewerCount = await api.get(`/viewers?event_id=${event.event_id}`)
-                    const racerCount = await api.get(`/racers?event_id=${event.event_id})`)
-                    const hostname = await api.get(`/events?event_id=${event.host_id}`)
+            //     for (const event of allEvents) {
+            //         const viewerCount = await api.get(`/viewers?event_id=${event.event_id}`)
+            //         const racerCount = await api.get(`/racers?event_id=${event.event_id})`)
+            //         const hostname = await api.get(`/events?event_id=${event.host_id}`)
     
-                    const eventWithCounts = {
-                        ...event,
-                        viewerCount: viewerCount,
-                        racerCount: racerCount,
-                        hostname: hostname
-                    }
+            //         const eventWithCounts = {
+            //             ...event,
+            //             viewerCount: viewerCount,
+            //             racerCount: racerCount,
+            //             hostname: hostname
+            //         }
     
-                    allEventsWithCount.push(eventWithCounts)
-                }
-    
-                setEvents(allEventsWithCount)
-            } catch (err) {
-                console.error(err)
-            }
-            // const mockEvents = [
-            //     {
-            //         event_id: 1,
-            //         event_name: "Test event",
-            //         hostname: "Test host",
-            //         date_time: "12:00pm Sunday",
-            //         price: "$2.00"
-            //     },
-            //     {
-            //         event_id: 2,
-            //         event_name: "test event",
-            //         hostname: "test host",
-            //         date_time: "12:00pm Sunday",
-            //         price: "$2.00"
-            //     },
-            //     {
-            //         event_id: 3,
-            //         event_name: "test event",
-            //         hostname: "test host",
-            //         date_time: "12:00pm Sunday",
-            //         price: "$2.00"
-            //     },
-            //     {
-            //         event_id: 4,
-            //         event_name: "test event",
-            //         hostname: "test host",
-            //         date_time: "12:00pm Sunday",
-            //         price: "$2.00"
-            //     },
-            //     {
-            //         event_id: 5,
-            //         event_name: "test event",
-            //         hostname: "test host",
-            //         date_time: "12:00pm Sunday",
-            //         price: "$2.00"
+            //         allEventsWithCount.push(eventWithCounts)
             //     }
-            // ]
+    
+            //     setEvents(allEventsWithCount)
+            // } catch (err) {
+            //     console.error(err)
+            // }
+            const mockEvents = [
+                {
+                    event_id: 1,
+                    event_name: "Test event",
+                    hostname: "Test host",
+                    date_time: "12:00pm Sunday",
+                    price: "$2.00"
+                },
+                {
+                    event_id: 2,
+                    event_name: "test event",
+                    hostname: "test host",
+                    date_time: "12:00pm Sunday",
+                    price: "$2.00"
+                },
+                {
+                    event_id: 3,
+                    event_name: "test event",
+                    hostname: "test host",
+                    date_time: "12:00pm Sunday",
+                    price: "$2.00"
+                },
+                {
+                    event_id: 4,
+                    event_name: "test event",
+                    hostname: "test host",
+                    date_time: "12:00pm Sunday",
+                    price: "$2.00"
+                },
+                {
+                    event_id: 5,
+                    event_name: "test event",
+                    hostname: "test host",
+                    date_time: "12:00pm Sunday",
+                    price: "$2.00"
+                }
+            ]
 
-            // setEvents(mockEvents)
+            setEvents(mockEvents)
 		}
 
         getEvents()
